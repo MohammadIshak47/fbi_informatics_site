4c2e8f66986bf184fba3bccc512d8e49484c6821
MohammadIshak47 <ishak1998bd@gmail.com>
2024-05-26 18:46:52 +0600

    updated

---
+++ consent.cookiebot.com/98d2548b-db51-4d0d-95b6-79fcce69e5e9/cc.js
@@ -0,0 +1,8 @@
+if (console) {
+    var cookiedomainwarning = 'Error: The domain INNOWISE.COM is not authorized to show the cookie banner for domain group ID 98d2548b-db51-4d0d-95b6-79fcce69e5e9. Please add it to the domain group in the Cookiebot Manager to authorize the domain.';
+    if (typeof console.warn === 'function') {
+        console.warn(cookiedomainwarning)
+    } else {
+        console.log(cookiedomainwarning)
+    }
+};
---
+++ js-eu1.hs-scripts.com/143644853.js
@@ -0,0 +1,40 @@
+var _hsp = window._hsp = window._hsp || [];
+_hsp.push(['addEnabledFeatureGates', []]);
+_hsp.push(['useV2Wildcard', true]);
+! function(t, e, r) {
+    if (!document.getElementById(t)) {
+        var n = document.createElement("script");
+        for (var a in n.src = "https://js-eu1.hs-banner.com/v2/143644853/banner.js", n.type = "text/javascript", n.id = t, r) r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
+        var i = document.getElementsByTagName("script")[0];
+        i.parentNode.insertBefore(n, i)
+    }
+}("cookieBanner-143644853", 0, {
+    "data-cookieconsent": "ignore",
+    "data-hs-ignore": true,
+    "data-loader": "hs-scriptloader",
+    "data-hsjs-portal": 143644853,
+    "data-hsjs-env": "prod",
+    "data-hsjs-hublet": "eu1"
+});
+! function(e, t) {
+    if (!document.getElementById(e)) {
+        var c = document.createElement("script");
+        c.src = "https://js-eu1.hs-analytics.net/analytics/1716725700000/143644853.js", c.type = "text/javascript", c.id = e;
+        var n = document.getElementsByTagName("script")[0];
+        n.parentNode.insertBefore(c, n)
+    }
+}("hs-analytics");
+! function(t, e, r) {
+    if (!document.getElementById(t)) {
+        var n = document.createElement("script");
+        for (var a in n.src = "https://js-eu1.hubspot.com/web-interactives-embed.js", n.type = "text/javascript", n.id = t, r) r.hasOwnProperty(a) && n.setAttribute(a, r[a]);
+        var i = document.getElementsByTagName("script")[0];
+        i.parentNode.insertBefore(n, i)
+    }
+}("hubspot-web-interactives-loader", 0, {
+    "crossorigin": "anonymous",
+    "data-loader": "hs-scriptloader",
+    "data-hsjs-portal": 143644853,
+    "data-hsjs-env": "prod",
+    "data-hsjs-hublet": "eu1"
+});
